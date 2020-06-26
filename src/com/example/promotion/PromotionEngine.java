64cf2a725654f950ed361f6ca8a404a94a42a42c
User6987 <thisisdarkman@gmail.com>
2020-06-26 14:17:16 +0530

    Created item class

--- src/com/example/promotion/PromotionEngine.java
+++ src/com/example/promotion/PromotionEngine.java
@@ -3,28 +3,32 @@ package com.example.promotion;
 import java.util.Scanner;
 
 public class PromotionEngine {
+	
+	public int executeCheckout() {
+		try {
+			int total = 0;
+			
+			return total;
+		}
+		catch(Exception e) {
+			System.out.print("Something went wrong!!");
+			return -1;
+		}
+		
+	}
 
 	public static void main(String[] args) {
 		try {
-			int total = 0;
-			Scanner in = new Scanner(System.in);
-			System.out.print("Enter total number of items in cart:: ");
-			int total_items = in.nextInt();
-				
-			if(total_items <=0) {
-				System.out.print("You have entered invalid input.");
-				System.exit(0);
-			}
-			else {
-				
-			}
-			System.out.println("Total cost of the cart after applying promo:: "+ total);
-			in.close();
+			PromotionEngine pe = new PromotionEngine();
+			int total = pe.executeCheckout();
+			if(total > 0)
+				System.out.print("Total cost of the cart after applying promo:: "+ total);
+			else
+				System.out.print("You have entered invalid input!!");
 		}
 		catch(Exception e) {
 			System.out.println(e);
 		}
-
 	}
 
 }
