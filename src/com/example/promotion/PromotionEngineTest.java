19ad65618c27cb60f22646431268ebd7361f5fd0
User6987 <thisisdarkman@gmail.com>
2020-06-26 16:04:00 +0530

    Created JUnit Test class

---
+++ src/com/example/promotion/PromotionEngineTest.java
@@ -0,0 +1,14 @@
+package com.example.promotion;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class PromotionEngineTest {
+
+	@Test
+	void testExecuteCheckout() {
+		PromotionEngine pe = new PromotionEngine();
+	}
+
+}
