c35d9ef9fafe2ff4301bb05e0ad13c5f0286b882
User6987 <thisisdarkman@gmail.com>
2020-06-26 15:50:00 +0530

    finding the min among multiple items to apply promo

--- src/com/example/promotion/CheckoutCart.java
+++ src/com/example/promotion/CheckoutCart.java
@@ -65,6 +65,20 @@ public class CheckoutCart {
 										});
 									}
 								}
+								else {
+									var wrapper3 = new Object(){ int min=-1; };
+
+									for(int j=0;j<temp.size();j++) {
+										String s = temp.get(j);
+										promo.promos.get(present).forEach((k2,v2) -> {
+											if(!k2.equals(s)) {
+												if((wrapper3.min>ci.items.get(s)/v2) || wrapper3.min<0) {
+													wrapper3.min = ci.items.get(s)/v2;
+												}
+											}
+										});
+									}
+								}
 							});	
 						}
 						else {
